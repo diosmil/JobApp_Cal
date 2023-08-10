db3ccc5f8844c353ff2b0d8e6775f01bb55ef0b4
diosmil <102917297+diosmil@users.noreply.github.com>
2023-08-10 19:23:11 -0400

    update

--- jop_app/lib/main.dart
+++ jop_app/lib/main.dart
@@ -5,10 +5,12 @@ import 'package:flutter/widgets.dart';
 
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
